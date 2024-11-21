351a2ae72edc8aa38e6e6147b54d11be7fd09994
Jessicavazm <jjessicavaz@gmail.com>
2024-11-21 22:22:01 +1100

    docs: Added comments to code

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import React, { Component } from 'react';
+// Import React to work with React
+// Import PokemonCard from PokemonCard Component
+import React, {Component} from 'react';
 import './App.css';
 import PokemonCard from './components/PokemonCard';
 
+// Export class, function or component you need to export it to the main file
+// export default App can be used towards the end to export it
 export default class App extends Component {
-  constructor(){
-    super();
-
-    this.state = {
+  constructor(){ // class needs a constructor method to initialise the class
+    super(); // super() is needed to call the constructor's functions and methods
+    // state variable defines the component state at some specific period
+    this.state = { 
       numberOfPokemon: 6
     }
   }
@@ -19,6 +23,8 @@ export default class App extends Component {
     console.log("Second Event Click");
   }
 
+  // Use setState method to update/ change variable state value
+  // Use arrow functions when creating functions inside classes in React
   decreasePokemonNumber = () => {
     if (this.state.numberOfPokemon > 0){
       this.setState({numberOfPokemon: this.state.numberOfPokemon - 1});
@@ -28,13 +34,18 @@ export default class App extends Component {
   increasePokemonNumber = () => {
     this.setState({numberOfPokemon: this.state.numberOfPokemon + 1});
   }
-
+  // Render function returns JSX component
+  // Render needs to be called in order to use the return function in class
+  // Use curly braces when you want to have a JS code inside HTML elements
+  // EventListener => camelCase, {} and 'this' kw are used
+  // Arrow fn when you want to have multiple functions for Event Listener, add () on each fn
+  // If only one function in event listener, don't call the ()
   render(){
     return(
       <>
         <h1>Hello World</h1>
         <p>My Pokemon Team</p>
-        <h5>Number of Pokemon: {this.state.numberOfPokemon} </h5>
+        <h5>Number of Pokemon: {this.state.numberOfPokemon} </h5> 
         <button onClick={this.decreasePokemonNumber}>Decrease Pokemon Amount</button>
         <button onClick={ () => {
           this.exampleFunc1();
@@ -44,9 +55,9 @@ export default class App extends Component {
         {
           // Creates an array of given length
           Array(this.state.numberOfPokemon)
-          // fills the array with null values
+          // Fills the array with null values
           .fill(null)
-          // on the empty array, map and place the component there.
+          // On the empty array, map and place the component there.
           .map((element, index) => {
             return <PokemonCard key={index}/>
           })
--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,5 +1,7 @@
+// Import React to work with React
 import React, { Component } from 'react';
 
+// export default to export PokemonCard component
 export default class PokemonCard extends Component {
     constructor(){
         super();
@@ -10,11 +12,14 @@ export default class PokemonCard extends Component {
         }
     }
 
+    // ComponentDidMount for when the component loads
+    // Fetch Pokemon 
     async componentDidMount(){
         let randomNumber = Math.ceil(Math.random() * 1025);
         let response = await fetch(`https://pokeapi.co/api/v2/pokemon/${randomNumber}`);
         let data = await response.json();
 
+        // update state variable value
         this.setState({
             pokemonName: data.name,
             pokemonImage: data.sprites.front_default
@@ -34,7 +39,8 @@ export default class PokemonCard extends Component {
                     // If its true, render this content
                     <>
                         <h1>{this.state.pokemonName}</h1>
-                        <img src={this.state.pokemonImage} />
+                        <img src={this.state.pokemonImage} alt={`Pokemon ${this.state.pokemonName}`} />
+
                     </>
                     :
                     // If its false, render this content
